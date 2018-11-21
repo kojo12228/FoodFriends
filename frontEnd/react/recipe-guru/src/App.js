4a68bff08ae11c9d4debd2cd1674936b62a732ae
mm33g15 <36045262+mm33g15@users.noreply.github.com>
2018-11-21 14:53:46 +0000

    react merge
    test

--- frontEnd/react/recipe-guru/src/App.js
+++ frontEnd/react/recipe-guru/src/App.js
@@ -1,25 +1,64 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './style.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+      <div>
+        <link rel="stylesheet" href="style.css" />
+        <title>Recipe Guru</title>
+        <link rel="shortcut icon" href="img/nameofyouriconfile.ico" />
+        {/*links this to a javascript file*/}
+        {/*h1 id="mainTitle">Recipe Guru</h1*/}
+        <a>
+          {/*img src="title2.png" alt="Recipe Guru"*/}
+        </a>
+        <h1 id="arTitle">Recipe Guru</h1>
+        {/*Search for ingredients search box + Add Ingredient button*/}
+        <div id="ingredientSearch">
+          {/*Search box*/}
+          <input id="search" type="text" placeholder="Search for ingredients..." />
+          {/*button*/}
+          <input type="submit" id="addIngr" defaultValue="+" />  
+        </div>
+        {/*Ingredients List/results*/}
+        <section id="ingredientsResults">
+          <h2 id="ingredientsLabel">Selected Ingredients</h2> 
+          <ul id="returnedIngredients">
+            <li><a href="#">Milk</a></li>
+            <li><a href="#">Flour</a></li>
+            <li><a href="#">Salt</a></li>
+            <li><a href="#">Pepper</a></li>
+            <li><a href="#">Milk</a></li>
+            <li><a href="#">Flour</a></li>
+            <li><a href="#">Salt</a></li>
+            <li><a href="#">Pepper</a></li>
+            <li><a href="#">Beans</a></li>
+            <li><a href="#">Asparagus</a></li>
+            <li><a href="#">Paprika</a></li>
+            <li><a href="#">Beans</a></li>
+            <li><a href="#">Asparagus</a></li>
+            <li><a href="#">Paprika</a></li>
+          </ul>
+        </section>
+        <div id="goGuru">
+          <input id="submit" defaultValue="GO Guru!"/>
+          <br />
+          <div id="list8">
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Blog</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </div>
+          <br />
+          <br />
+          <a>
+            <img src="logo.png" id="rgLogo" alt="Recipe Guru" />
           </a>
-        </header>
+        </div>  
       </div>
     );
   }
