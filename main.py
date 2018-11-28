2edac413b3c75800de468cda038c8ed0cbb826ab
Sakib Khahar <sakib.khahar@hotmail.co.uk>
2018-11-28 15:06:43 +0000

    Adding to database

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, render_template, request, json, jsonify
+from google.appengine.ext import ndb
 
 app = Flask(__name__)
 
@@ -13,5 +14,39 @@ def getIngredients():
     jsonfilepath = os.path.join(app.root_path, 'data', "ingrlist.json")
     # From https://stackoverflow.com/a/21230727
     ingredientList = json.load(open(jsonfilepath))
-    return jsonify(result=ingredientList)
     # See http://flask.pocoo.org/docs/1.0/patterns/jquery/ for using jQuery and Flask
+
+@app.route('/testjsonhtml')
+def testjsonhtml():
+    return render_template('testjson.html')
+
+@app.route('/testjson')
+def testjson():
+    jsonfilepath = os.path.join(app.root_path, 'data', "combined_r_i.json")
+    recipes = json.load(open(jsonfilepath))
+    recipeModels = [ Recipe(
+            directions = recipe["directions"],
+            fat = recipe["fat"],
+            categories = recipe["categories"],
+            calories = recipe["calories"],
+            protein = recipe["protein"],
+            rating = recipe["rating"],
+            title = recipe["title"],
+            ingredients = recipe["ingredients"],
+            ingredientNames = recipe["ingredientNames"],
+            sodium = recipe["sodium"]
+        ) for id, recipe in recipes.iteritems() ]
+    for rm in recipeModels:
+        rm.put()
+
+class Recipe(ndb.Model):
+    directions = ndb.StringProperty(repeated=True)
+    fat = ndb.IntegerProperty()
+    categories = ndb.StringProperty(repeated=True)
+    calories = ndb.IntegerProperty()
+    protein = ndb.IntegerProperty()
+    rating= ndb.FloatProperty()
+    title = ndb.StringProperty()
+    ingredients = ndb.StringProperty(repeated=True)
+    ingredientNames = ndb.StringProperty(repeated=True)
+    sodium = ndb.IntegerProperty()                                                                        
