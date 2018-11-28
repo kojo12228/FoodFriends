25f5c642657fa66c36fa6eeee8ff20194252c7b2
Kojo Amoasi <kojo12331@hotmail.co.uk>
2018-11-28 23:55:08 +0000

    Proposed recipe API, untested

--- main.py
+++ main.py
@@ -15,29 +15,30 @@ def getIngredients():
     # From https://stackoverflow.com/a/21230727
     ingredientList = json.load(open(jsonfilepath))
     # See http://flask.pocoo.org/docs/1.0/patterns/jquery/ for using jQuery and Flask
+    return jsonify(result=ingredientList)
 
-@app.route('/testjsonhtml')
-def testjsonhtml():
-    return render_template('testjson.html')
+# @app.route('/testjsonhtml')
+# def testjsonhtml():
+#     return render_template('testjson.html')
 
-@app.route('/testjson')
-def testjson():
-    jsonfilepath = os.path.join(app.root_path, 'data', "combined_r_i.json")
-    recipes = json.load(open(jsonfilepath))
-    recipeModels = [ Recipe(
-            directions = recipe["directions"],
-            fat = recipe["fat"],
-            categories = recipe["categories"],
-            calories = recipe["calories"],
-            protein = recipe["protein"],
-            rating = recipe["rating"],
-            title = recipe["title"],
-            ingredients = recipe["ingredients"],
-            ingredientNames = recipe["ingredientNames"],
-            sodium = recipe["sodium"]
-        ) for id, recipe in recipes.iteritems() ]
-    for rm in recipeModels:
-        rm.put()
+# @app.route('/testjson')
+# def testjson():
+#     jsonfilepath = os.path.join(app.root_path, 'data', "combined_r_i.json")
+#     recipes = json.load(open(jsonfilepath))
+#     recipeModels = [ Recipe(
+#             directions = recipe["directions"],
+#             fat = recipe["fat"],
+#             categories = recipe["categories"],
+#             calories = recipe["calories"],
+#             protein = recipe["protein"],
+#             rating = recipe["rating"],
+#             title = recipe["title"],
+#             ingredients = recipe["ingredients"],
+#             ingredientNames = recipe["ingredientNames"],
+#             sodium = recipe["sodium"]
+#         ) for id, recipe in recipes.iteritems() ]
+#     for rm in recipeModels:
+#         rm.put()
 
 class Recipe(ndb.Model):
     directions = ndb.StringProperty(repeated=True)
@@ -49,4 +50,23 @@ class Recipe(ndb.Model):
     title = ndb.StringProperty()
     ingredients = ndb.StringProperty(repeated=True)
     ingredientNames = ndb.StringProperty(repeated=True)
-    sodium = ndb.IntegerProperty()                                                                        
+    sodium = ndb.IntegerProperty()
+
+def modelToDictionary(recipe):
+    return {
+        "directions": recipe.directions,
+        "fat": recipe.fat,
+        "categories": recipe.categories,
+        "calories": recipe.protein,
+        "rating": recipe.rating,
+        "title": recipe.title,
+        "ingredients": recipe.ingredients,
+        "sodium": recipe.sodium
+    }
+
+@app.route("/recipes")
+def recipeNoFilter():
+    ingredients = request.args.getlist("ing[]")
+    recipes = Recipe.query()
+    matchingRecipes = [ modelToDictionary(recipe) for recipe in recipes.iter() if set(ingredients) <= set(recipe.ingredientNames) ]
+    return jsonify(matchingRecipes)
