b802008795511556f7b82cfaca1cd9af6e2cd903
Kojo Amoasi <kojo12331@hotmail.co.uk>
2018-12-05 13:37:52 +0000

    Enable CORS using Flask-CORS library

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask, render_template, request, json, jsonify
 from google.appengine.ext import ndb
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def form():
