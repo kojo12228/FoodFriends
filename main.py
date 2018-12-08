efda643b658026e31cf74c55c7bde2bb7ee0d91d
Kojo Amoasi <kojo12331@hotmail.co.uk>
2018-12-08 12:53:01 +0000

    Support options for recipes - only dislay recipes with at least ingredients requested - only display recipes with at most ingredients requested

--- main.py
+++ main.py
@@ -10,37 +10,14 @@ CORS(app)
 def form():
     return render_template('index.html')
 
-@app.route('/ingredients')
+@app.route('/api/v1/ingredients')
 def getIngredients():
     # Based on code from https://stackoverflow.com/a/50935393
     jsonfilepath = os.path.join(app.root_path, 'data', "ingrlist.json")
     # From https://stackoverflow.com/a/21230727
     ingredientList = json.load(open(jsonfilepath))
     # See http://flask.pocoo.org/docs/1.0/patterns/jquery/ for using jQuery and Flask
-    return jsonify(result=ingredientList)
-
-# @app.route('/testjsonhtml')
-# def testjsonhtml():
-#     return render_template('testjson.html')
-
-# @app.route('/testjson')
-# def testjson():
-#     jsonfilepath = os.path.join(app.root_path, 'data', "combined_r_i.json")
-#     recipes = json.load(open(jsonfilepath))
-#     recipeModels = [ Recipe(
-#             directions = recipe["directions"],
-#             fat = recipe["fat"],
-#             categories = recipe["categories"],
-#             calories = recipe["calories"],
-#             protein = recipe["protein"],
-#             rating = recipe["rating"],
-#             title = recipe["title"],
-#             ingredients = recipe["ingredients"],
-#             ingredientNames = recipe["ingredientNames"],
-#             sodium = recipe["sodium"]
-#         ) for id, recipe in recipes.iteritems() ]
-#     for rm in recipeModels:
-#         rm.put()
+    return jsonify(ingredientList)
 
 class Recipe(ndb.Model):
     directions = ndb.StringProperty(repeated=True)
@@ -66,14 +43,67 @@ def modelToDictionary(recipe):
         "sodium": recipe.sodium
     }
 
-@app.route("/recipes/get")
-def recipeNoFilter():
+@app.route("/api/v1/recipes")
+def getRecipe():
     ingredients = request.args.getlist("ing[]")
     ingredientsEx = request.args.getlist("ingexc[]")
+
+    allergies = request.args.getlist("allergy[]")
+    diet = request.args.get("diet")
+    if (len(allergies) > 0):
+        jsonfilepath = os.path.join(app.root_path, 'data', "allergy.json")
+        allergyJSON = json.load(open(jsonfilepath))
+        for allergy in allergies:
+            ingredientsEx = ingredientsEx + allergyJSON[allergy]
+    if (not diet == None):
+        jsonfilepath = os.path.join(app.root_path, 'data', "dietary.json")
+        dietaryJSON = json.load(open(jsonfilepath))
+        if (diet[0] == "Vgn"):
+            ingredientsEx = ingredientsEx + dietaryJSON['Vegan'] + dietaryJSON['Meat'] + dietaryJSON['Fish'] + dietaryJSON['Egg'] + dietaryJSON['Dairy']
+        elif (diet[0] == "Vgt"):
+            ingredientsEx = ingredientsEx + dietaryJSON['Vegetarian'] + dietaryJSON['Meat'] + dietaryJSON['Fish']
+        elif (diet[0] == "Pesc"):
+            ingredientsEx = ingredientsEx + dietaryJSON['Pescatarian'] + dietaryJSON['Meat']
+    
     recipes = Recipe.query()
     includeRecipes = [ recipe for recipe in recipes.iter() if set(ingredients) <= set(recipe.ingredientNames) ] # <= is subset/equal to
     matchingRecipes = [ modelToDictionary(recipe) for recipe in includeRecipes if len(set(recipe.ingredientNames) & set(ingredientsEx)) == 0 ] # & is intersection
     return jsonify(matchingRecipes)
+
+@app.route("/api/test/recipes")
+def recipeNoFilter():
+    ingredients = request.args.getlist("ing[]")
+    ingredientsEx = request.args.getlist("ingexc[]")
+
+    allergies = request.args.getlist("allergy[]")
+    diet = request.args.get("diet")
+    if (len(allergies) > 0):
+        jsonfilepath = os.path.join(app.root_path, 'data', "allergy.json")
+        allergyJSON = json.load(open(jsonfilepath))
+        for allergy in allergies:
+            ingredientsEx = ingredientsEx + allergyJSON[allergy]
+    if (not diet == None):
+        jsonfilepath = os.path.join(app.root_path, 'data', "dietary.json")
+        dietaryJSON = json.load(open(jsonfilepath))
+        if (diet[0] == "Vgn"):
+            ingredientsEx = ingredientsEx + dietaryJSON['Vegan'] + dietaryJSON['Meat'] + dietaryJSON['Fish'] + dietaryJSON['Egg'] + dietaryJSON['Dairy']
+        elif (diet[0] == "Vgt"):
+            ingredientsEx = ingredientsEx + dietaryJSON['Vegetarian'] + dietaryJSON['Meat'] + dietaryJSON['Fish']
+        elif (diet[0] == "Pesc"):
+            ingredientsEx = ingredientsEx + dietaryJSON['Pescatarian'] + dietaryJSON['Meat']
+    
+    match = request.args.get("match")
+
+    recipes = Recipe.query()
+    includeRecipes = [ recipe for recipe in recipes.iter() if matchRecipes(match, ingredients, recipe.ingredientNames) ] # <= is subset/equal to
+    matchingRecipes = [ modelToDictionary(recipe) for recipe in includeRecipes if len(set(recipe.ingredientNames) & set(ingredientsEx)) == 0 ] # & is intersection
+    return jsonify(matchingRecipes)
+
+def matchRecipes(matchQueryValue, ingredients, recipeIngredientNames):
+    if (matchQueryValue == "Atleast" or matchQueryValue == None):
+        return set(ingredients) <= set(recipeIngredientNames)
+    elif (matchQueryValue == "Only"):
+        return set(ingredients) >= set(recipeIngredientNames)
 
 # For React Routing
 # Snippet derived from http://flask.pocoo.org/snippets/57/
