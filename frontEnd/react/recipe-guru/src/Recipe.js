a87f98864020ea6981f0db008865b522bf9adb2c
Kojo Amoasi <kojo12331@hotmail.co.uk>
2018-12-17 23:47:16 +0000

    Improve styling of recipe page - Separate recipe stat into separate component - Improve element id and class consistency

--- frontEnd/react/recipe-guru/src/Recipe.js
+++ frontEnd/react/recipe-guru/src/Recipe.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import * as $ from 'jquery'
+import $ from 'jquery';
 
 export class Recipe extends Component {
     constructor(props) {
@@ -22,72 +22,35 @@ export class Recipe extends Component {
         $.getJSON("https://recipe-guru.appspot.com/api/v1/recipe/" + this.props.id, (data) => {
             this.setState(data)
         })
-        // this.setState(
-        //     {
-        //         calories: 2,
-        //         categories: [
-        //             "Salad",
-        //             "Dairy",
-        //             "No-Cook",
-        //             "Yogurt",
-        //             "Mint",
-        //             "Cucumber",
-        //             "Summer",
-        //             "Bon Appétit",
-        //             "Kansas"
-        //         ],
-        //         directions: [
-        //             "Place cucumbers in colander. Sprinkle with salt. Let stand 15 minutes. Rinse cucumbers. Drain on paper towels. Combine mint, garlic, and oregano in large bowl. Stir in yogurt and cream. Add cucumber and stir to coat. Season with pepper. Refrigerate until chilled. (Can be prepared 5 hours ahead.)"
-        //         ],
-        //         fat: 3,
-        //         id: 4856496185671680,
-        //         ingredients: [
-        //             "3 medium cucumbers, peeled, seeded, diced",
-        //             "1 1/2 teaspoons salt",
-        //             "1 tablespoon fresh mint, minced",
-        //             "1 garlic clove, minced",
-        //             "1 teaspoon dried oregano",
-        //             "1 8-ounce container plain yogurt",
-        //             "2 tablespoons whipping cream",
-        //             "Pepper"
-        //         ],
-        //         rating: 3.75,
-        //         sodium: 603,
-        //         title: "Cucumber-Yogurt Salad with Mint "
-        //     }
-        // )
     }
 
     render() {
         const directionItems = this.state.directions.map(
-            (direction, index) => <li key={index}>{direction}</li>
+            (direction, index) => <li className="directionItem" key={index}>{direction}</li>
         );
         const ingredientItems = this.state.ingredients.map(
-            (ingredient, index) => <li key={index}>{ingredient}</li>
+            (ingredient, index) => <li className="ingredientItem" key={index}>{ingredient}</li>
         );
         return(
-            <div>
-                <div>
-                    <img src={ require('./logo.png') } alt="logo" />
-                    <h1>{this.state.title}</h1>
+            <div id="recipe">
+                <div id="recipeHeader">
+                    <img src={ require('./logo.png') } alt="logo" id="headerLogo" height="100" width="100"/>
+                    <h1 id="recipeName">{this.state.title}</h1>
                     <Rating rating={this.state.rating}/>
                 </div>
-                <h2>Ingredients</h2>
-                <ul>{ingredientItems}</ul>
-                <h2>Directions</h2>
-                <ol>{directionItems}</ol>
-                <h2>Stats</h2>
-                <div>
-                    <p>Calories</p>
-                    <p>{this.state.calories}</p>
+                <div className="recipeList">
+                    <h2 className="recipeSubHeading">Ingredients</h2>
+                    <ul>{ingredientItems}</ul>
                 </div>
-                <div>
-                    <p>Fat</p>
-                    <p>{this.state.fat}</p>
+                <div className="recipeList">
+                    <h2 className="recipeSubHeading">Directions</h2>
+                    <ol>{directionItems}</ol>
                 </div>
-                <div>
-                    <p>Sodium</p>
-                    <p>{this.state.sodium}</p>
+                <div id="stats">
+                    <h2 className="recipeSubHeading">Stats</h2>
+                    <StatBox heading="Calories" value={this.state.calories} />
+                    <StatBox heading="Fat" value={this.state.fat} />
+                    <StatBox heading="Sodium" value={this.state.sodium} />
                 </div>
             </div>
         )
@@ -95,7 +58,23 @@ export class Recipe extends Component {
 }
 
 function Rating(props) {
-    return <p>Rating: {props.rating}</p>
+    let rating = ""
+    for (let index = 0; index < Math.round(props.rating); index++) {
+        rating = rating + "\u25CF"
+    }
+    if (Math.round((props.rating % 1) * 2) === 1) {
+        rating = rating + "\u25D6"
+    } 
+    return <p id="recipeRating">Rating: {rating}</p>
+}
+
+function StatBox(props) {
+    return (
+        <span className="statBox">
+            <div className="statHeading"><h3>{props.heading}</h3></div>
+            <div className="statValue"><p>{props.value}</p></div>
+        </span>
+    )
 }
 
 export default Recipe;
