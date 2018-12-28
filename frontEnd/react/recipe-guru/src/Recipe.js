ac6ee0edcf6237fee58b9e5c89abc20da7a36ede
Kojo Amoasi <kojo12331@hotmail.co.uk>
2018-12-28 21:30:45 +0000

    Update Recipe page to use Results style shadows and animation

--- frontEnd/react/recipe-guru/src/Recipe.js
+++ frontEnd/react/recipe-guru/src/Recipe.js
@@ -36,21 +36,22 @@ export class Recipe extends Component {
             <div>
                 <link rel="stylesheet" href="Recipe.css" />
                 <div id="recipe">
-                    <div id="recipeHeader">
+                    <div id="recipeHeader" className="card">
                         <img src={ require('./logo.png') } alt="logo" id="headerLogo" height="100" width="100"/>
                         <h1 id="recipeName">{this.state.title}</h1>
                         <Rating rating={this.state.rating}/>
                     </div>
-                    <div className="recipeList">
+                    <div className="card recipeList">
                         <h2 className="recipeSubHeading">Ingredients</h2>
+                        <hr></hr>
                         <ul>{ingredientItems}</ul>
                     </div>
-                    <div className="recipeList">
+                    <div className="card recipeList">
                         <h2 className="recipeSubHeading">Directions</h2>
+                        <hr></hr>
                         <ol>{directionItems}</ol>
                     </div>
                     <div id="stats">
-                        <h2 className="recipeSubHeading">Stats</h2>
                         <StatBox heading="Calories" value={this.state.calories} />
                         <StatBox heading="Fat" value={this.state.fat} />
                         <StatBox heading="Sodium" value={this.state.sodium} />
@@ -71,8 +72,9 @@ function Rating(props) {
 
 function StatBox(props) {
     return (
-        <span className="statBox">
+        <span className="card statBox">
             <div className="statHeading"><h3>{props.heading}</h3></div>
+            <hr></hr>
             <div className="statValue"><p>{props.value}</p></div>
         </span>
     )
