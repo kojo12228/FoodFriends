86afcbaf7497ba3bdd6f24b613ccc2786f9abf5f
Kojo Amoasi <kojo12331@hotmail.co.uk>
2018-12-29 23:57:26 +0000

    Unfinished implementation of search - Problems: - Not implemented removal of selected items - Not implemented exclusion list - Not implemented go guru button - Not removed add ingredient button, but now redundant

--- frontEnd/react/recipe-guru/src/App.js
+++ frontEnd/react/recipe-guru/src/App.js
@@ -4,6 +4,7 @@ import Router from 'react-router-component'
 import './style.css';
 import Recipe from './Recipe.js'
 import Results from './Results.js'
+import Dataset from './Dataset'
 
 var Locations = Router.Locations;
 var Location = Router.Location;
@@ -21,7 +22,66 @@ class App extends Component {
 }
 
 class SearchHome extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ingredients: [],
+      matchedIngredients: [],
+      selectedIngredients: [],
+      input: "",
+      selected: false
+    };
+  }
+
+  componentDidMount() {
+    Dataset.getIngredients((data) => {
+      //const items = data.map((val, i) => {return {id: i, value: val}})
+      this.setState({ingredients: data})
+    })
+  }
+
+  textChanged(e) {
+    const inputRegex = new RegExp(e.target.value, "i")
+    const matchingIng = this.state.ingredients.filter((ingredient) => {
+      return ingredient.match(inputRegex)
+    })
+    this.setState({
+      input: e.target.value,
+      matchedIngredients: matchingIng
+    });
+  }
+
+  suggestionSelected(suggestion) {
+    this.setState({
+      selectedIngredients: this.state.selectedIngredients.concat([suggestion]),
+      input: "",
+      matchedIngredients: []
+    })
+  }
+
+  toggleFocus() {
+    this.setState({selected: !this.state.selected})
+  }
+ 
+  
+
   render() {
+    let selectedIngredients = this.state.selectedIngredients.map(ing => {
+      return (
+        <div key={ing} className="selectedIng">
+          <button className="removeIng">{"\u274C"}</button>
+          <p>{ing}</p>
+        </div>
+      )
+    })
+    let suggestedIngredients = this.state.matchedIngredients.map(ing => {
+      return (
+          <div key={ing}
+              className="suggestedItem"
+              onClick={() => this.suggestionSelected(ing)}>{ing}</div>)
+    })
+
     return (
       <div>
         <link rel="stylesheet" href="style.css" />
@@ -37,23 +97,30 @@ class SearchHome extends Component {
         {/*Search for ingredients search box + Add Ingredient button*/}
         <div className="ingredientSearch">
           {/*Search box*/}
-          <input id="search" type="text" placeholder="Search for ingredients..." size={20} />
+          <input id="search" type="text" placeholder="Search for ingredients..." size={20} 
+                  onChange={this.textChanged.bind(this)}
+                  onFocus={() => this.toggleFocus()}
+                  onBlur={() => this.toggleFocus()}
+                  value={this.state.input}/>
           {/*button*/}
           <button type="button" id="addIngr">+</button>
         </div>
         {/*Ingredients List/results*/}
-        <div className="ingredientsResults">
+        {/*<div className="ingredientsResults">
           <h2 id="ingredientsLabel">Selected Ingredients</h2> 
           {/*       <div class="left area" >
         <span id= "ingr_elem">
           Selected Ingredients:
-        </span> */}
+        </span> }
+
         </div>
         <ol id="returnedIngredients">
           <li><a href="#">Milk</a></li>
           <li><a href="#">Asparagus</a></li>
           <li><a href="#">Paprika</a></li>
-        </ol>
+        </ol>*/}
+        <div style={{overflowY: "scroll", maxHeight: "250px", width: "50%", margin: "auto"}}>{suggestedIngredients}</div>
+        <div style={{width: "50%", margin: "auto"}}>{selectedIngredients}</div>
         <div id="goGuru">
           <button type="button" id="submit">GO GURU!</button>
         </div>
