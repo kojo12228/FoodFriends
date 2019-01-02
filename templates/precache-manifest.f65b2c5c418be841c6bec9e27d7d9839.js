ab2bfc84f4cbe3d16ab3fff35b937624aadfbb6a
Kojo Amoasi <kojo12331@hotmail.co.uk>
2019-01-02 21:15:31 +0000

    Built the application for deployment on GAE

---
+++ templates/precache-manifest.f65b2c5c418be841c6bec9e27d7d9839.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f251fc8595b56595120d0b32a5eaf18",
+    "url": "/static/media/logo.3f251fc8.png"
+  },
+  {
+    "revision": "f366d23787e2f3a3986d",
+    "url": "/static/css/main.7c6a7aff.chunk.css"
+  },
+  {
+    "revision": "bea18bdf2a3ab04ca5b5036340f61dca",
+    "url": "/static/media/ARBONNIE.bea18bdf.ttf"
+  },
+  {
+    "revision": "17669a1b6764e068c1a2",
+    "url": "/static/js/1.17669a1b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f366d23787e2f3a3986d",
+    "url": "/static/js/main.f366d237.chunk.js"
+  },
+  {
+    "revision": "08f821af6c808855dd27d6c19849bbe0",
+    "url": "/static/media/ARBONNIE.08f821af.eot"
+  },
+  {
+    "revision": "228416b76de63d7e4a6f59487c2bf79b",
+    "url": "/static/media/ARBONNIE.228416b7.woff"
+  },
+  {
+    "revision": "e3316d22333ebe9587714f902fd57167",
+    "url": "/static/media/ARBONNIE.e3316d22.svg"
+  },
+  {
+    "revision": "17669a1b6764e068c1a2",
+    "url": "/static/css/1.dd7743e8.chunk.css"
+  },
+  {
+    "revision": "1cd96277b6dbbd6f90528aaedae71c7e",
+    "url": "/index.html"
+  }
+];
